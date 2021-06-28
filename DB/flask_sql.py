02225772b25a81d681f9729a55af91b272f7a28d
MidhunM <midhun5511@gmail.com>
2021-06-28 18:27:53 +0530

    Used WT Forms

--- DB/flask_sql.py
+++ DB/flask_sql.py
@@ -1,12 +1,54 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, \
+    SubmitField
+from wtforms.validators import ValidationError, DataRequired, \
+    Email, EqualTo, Length
 
 app = Flask (__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] = "random string"
 
 db = SQLAlchemy(app)
+
+
+
+class CreateUserForm(FlaskForm):
+    username = StringField(label=('Username'), 
+        validators=[DataRequired(), 
+        Length(max=64)])
+    email = StringField(label=('Email'), 
+        validators=[DataRequired(), 
+        Email(), 
+        Length(max=120)])
+    password = PasswordField(label=('Password'), 
+        validators=[DataRequired(), 
+        Length(min=2, message='Password should be at least %(min)d characters long')])
+    confirm_password = PasswordField(
+        label=('Confirm Password'), 
+        validators=[DataRequired(message='*Required'),
+        EqualTo('password', message='Both password fields must be equal!')])
+
+    receive_emails = BooleanField(label=('Receive merketting emails.'))
+
+    submit = SubmitField(label=('Submit'))
+
+
+    def validate_username(self,username):
+       if '%' in username.data:
+          raise ValidationError("Character % is not allowed in username")
+
+
+@app.route('/wtf', methods=('GET', 'POST'))
+def wtf_sample():
+    form = CreateUserForm()
+    if form.validate_on_submit():
+        return f'''<h1> Welcome {form.username.data} </h1>'''
+    return render_template('forms.html', form=form)
+
+
+
 class Students(db.Model):
    id   = db.Column('student_id', db.Integer, primary_key = True)
    name = db.Column(db.String(100))
@@ -58,7 +100,6 @@ def new():
          flash('Record was successfully added')
          return redirect(url_for('show_all'))
    return render_template('new.html')
-
 
 if __name__ == '__main__':
    db.create_all()
